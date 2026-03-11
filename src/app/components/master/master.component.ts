19147bc6b9713a17ba75791cba5d462554d326d6
skjinagal <souravkumar9991sk@gmail.com>
2026-03-11 15:31:14 +0530

    structural directive using *ngIf

---
+++ src/app/components/master/master.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RolesComponent } from '../roles/roles.component';
+import { DesignationComponent } from '../designation/designation.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-master',
+  imports: [RolesComponent, DesignationComponent, CommonModule],
+  templateUrl: './master.component.html',
+  styleUrl: './master.component.css'
+})
+export class MasterComponent {
+  currentComponent: string = '';
+  showRoles() {
+    this.currentComponent = 'roles';
+  }
+
+  showDesignation() {
+    this.currentComponent = 'designation';
+  }
+}
