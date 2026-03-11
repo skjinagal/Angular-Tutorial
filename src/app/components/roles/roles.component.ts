672d36fa6f68759dd301f9e465c4ae5a4bf9b9b0
skjinagal <souravkumar9991sk@gmail.com>
2026-03-11 17:15:36 +0530

    directives, added table iterated over roles, setup json server

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,23 +1,43 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../models/interfaces/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
-  name: string = 'John Doe';
-  department: string = 'Engineering';
-  JoinDate: Date = new Date()
-  inputType = 'button';
-  selectedRole: string = '';
-  showWelcomeAlert() {
-    alert(`Welcome, ${this.name} from ${this.department} department!`);
+export class RolesComponent implements OnInit {
+
+  roles: IRole[] = [];
+
+  ngOnInit(): void
+  {
+    this.getAllRoles();
   }
 
-  showMessage(message: string) {
-    alert(message);
+  httpClient = inject(HttpClient);
+
+  getAllRoles() {
+    this.httpClient.get('http://localhost:3000/roles').subscribe((data) => {
+      this.roles = data as IRole[];
+      console.log(this.roles);
+    });
   }
+  // name: string = 'John Doe';
+  // department: string = 'Engineering';
+  // JoinDate: Date = new Date()
+  // inputType = 'button';
+  // selectedRole: string = '';
+  // showWelcomeAlert() {
+  //   alert(`Welcome, ${this.name} from ${this.department} department!`);
+  // }
+
+  // showMessage(message: string) {
+  //   alert(message);
+  // }
+
 }
